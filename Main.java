7cc844e518ac199168d19b1e4386ddda8a8fe212
DNazzal <97950944+DNazzal@users.noreply.github.com>
2022-06-21 22:27:18 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,75 +1,38 @@
-package hotel;
+package university;
 
 public class Main {
-
     public static void main(String[] args) {
+        // Initialize the repository
+        University university = new University(10);
+// Initialize entities
+        Student student = new Student("John", "Smith", "Astrology");
+        Student studentTwo = new Student("Anna", "Cameron", "Geometry");
+        Student studentThree = new Student("Samy", "Johnson", "Algebra");
+        Student studentFour = new Student("Rihanna", "Fenty", "Music");
+        Student studentFive = new Student("Ellie", "Goulding", "Music");
+// Print Student
+        System.out.println(student);
+        // Student: John Smith, Astrology
+// Register Student
+        String register = university.registerStudent(student);
+        System.out.println(university.getCapacity()); // 10
+        System.out.println(register); // Added student John Smith
+        String registerTwo = university.registerStudent(studentTwo);
+        String registerThree = university.registerStudent(studentThree);
+        String registerFour = university.registerStudent(studentFour);
+// Dismiss Student
+        String dismissed = university.dismissStudent(student);
+        System.out.println(dismissed); // Removed student John Smith
+        String dismissedTwo = university.dismissStudent(studentFive);
+        System.out.println(dismissedTwo); // Student not found
+// Get Student
+        System.out.println(university.getStudent("Rihanna", "Fenty"));
+// Student: Rihanna Fenty, Music
+        System.out.println(university.getStudentCount()); // 3
+        System.out.println(university.getStatistics());
+//==Student: First Name = Anna, Last Name = Cameron, Best Subject = Geometry
+        //==Student: First Name = Samy, Last Name = Johnson, Best Subject = Algebra
+        //==Student: First Name = Rihanna, Last Name = Fenty, Best Subject = Music
 
-//Initialize the repository (hotel)
-
-        Hotel hotel = new Hotel("Hilton", 30);
-
-//Initialize entity
-
-        Person person = new Person("Mark", 11111, 41, "Sofia");
-
-//Print person
-
-        System.out.println(person);
-
-//Person Mark: 11111, Age: 41, Hometown: Sofia
-
-//Add person
-
-        hotel.add(person);
-
-        System.out.println(hotel.getCount()); //1
-
-        System.out.println(hotel.remove("Anna")); //false
-
-        Person firstPerson = new Person("Alice", 22121, 18, "London");
-
-        Person secondPerson= new Person("Lizzy", 31311, 24, "Varna");
-
-        Person thirdPerson = new Person("Lucy", 54122, 31, "Birmingham");
-
-        Person fourthPerson = new Person("Maria", 66611, 41, "Sofia");
-
-//Add people
-
-        hotel.add(firstPerson);
-
-        hotel.add(secondPerson);
-
-        hotel.add(thirdPerson);
-
-        hotel.add(fourthPerson);
-
-//// Get person
-//
-        Person personForGet = hotel.getPerson("Lucy", "Birmingham");
-
-        Person personForGet1 = hotel.getPerson("Anna", "Burgas");
-
-        System.out.println(personForGet); // Person Lucy: 54122, Age: 31, Hometown:
-
-      //  Birmingham
-
-        System.out.println(personForGet1); // null
-//
-////Count
-//
-        System.out.println(hotel.getCount()); // 5
-//
-////Remove Person
-//
-        System.out.println(hotel.remove("Alice"));
-        //true
-        // Get Statistics
-         System.out.println(hotel.getStatistics());
-        // The people in the hotel Hilton are:
-        // Person Mark: 11111, Age: 41, Hometown: Sofia
-        // Person Lizzy: 31311, Age: 24, Hometown: Varna
-        // Person Lucy: 54122, Age: 31, Hometown: Birmingham
-        // Person Maria: 66611, Age: 41, Hometown: Sofia
-        }
+    }
 }
